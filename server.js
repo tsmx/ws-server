b2510c1a3e6279f0d14bd1ce041b0c67c3d3f5b1
tsmx <strs@freenet.de>
2022-02-28 20:07:24 +0100

    Mongoose document save error handling added

--- server.js
+++ server.js
@@ -7,17 +7,24 @@ var app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
+// for Wunderground protocol type
 app.get('/data', (req, res) => {
     console.log(new Date().toISOString(), 'GET /data called from', req.ip, 'data:', JSON.stringify(req.query));
     res.sendStatus(200);
 });
 
+// for Ecowitt protocol type
 app.post('/data', (req, res) => {
     console.log(new Date().toISOString(), 'POST /data called from', req.ip, 'data:', JSON.stringify(req.body));
-    persist.fromPostBody(req.body).then((doc) => { 
-        logger.info('Weather data saved with ObjectID: ' + doc.id);
-        res.sendStatus(200);
-    });
+    persist.fromPostBody(req.body)
+        .then((doc) => {
+            logger.info('Weather data saved with ObjectID: ' + doc.id);
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            logger.error('Error while saving document: ' + err.message);
+            res.sendStatus(500);
+        });
 });
 
 module.exports = app;
