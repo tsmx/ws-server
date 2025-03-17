f00beb6ee4171f8fe7cfe859be7d45fee86d9b80
tsmx <dev@tsmx.net>
2025-03-17 21:53:37 +0100

    eslint upgraded to v9

--- utils/numbers.js
+++ utils/numbers.js
@@ -1,9 +1,9 @@
 module.exports.roundToOne = function (num) {
     if (isNaN(num) || num === null) return null;
-    return +(Math.round(num + "e+1") + "e-1");
-}
+    return +(Math.round(num + 'e+1') + 'e-1');
+};
 
 module.exports.roundToZero = function (num) {
     if (isNaN(num) || num === null) return null;
     return Math.round(num);
-}
+};
